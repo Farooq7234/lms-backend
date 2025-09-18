ae49f6fe41bd03ad94dff1093350568dc2e07e94
farooq <ufservices465@gmail.com>
2025-09-18 22:46:21 +0530

    Refine CORS configuration to allow specific origins for enhanced security

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://lms-erino.vercel.app"], credentials: true }));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
